57320f2c213e387332072bb90f0cd0557a64750a
AndrewShakula <attentionhub@gmail.com>
2022-07-20 04:26:51 +0300

    §wip

--- macpaw/Views/ContentView.swift
+++ macpaw/Views/ContentView.swift
@@ -11,20 +11,67 @@ struct ContentView: View {
     
     @ObservedObject var datasetManager = DatasetManager()
     
+    init() {
+        UITableView.appearance().backgroundColor = .clear
+        UITableViewCell.appearance().backgroundColor = .clear
+        
+        // NavigationBar appereance settup
+//        let navBarAppereance = UINavigationBarAppearance()
+//        navBarAppereance.titleTextAttributes = [.foregroundColor: UIColor.systemBackground]
+//        navBarAppereance.largeTitleTextAttributes = [.foregroundColor: UIColor.systemBackground]
+//        navBarAppereance.backgroundColor = UIColor.clear
+//        //navBarAppereance.backgroundEffect = UIBlurEffect(style: UIBlurEffect.Style.dark)
+//        navBarAppereance.shadowColor = UIColor.clear
+//        UINavigationBar.appearance().standardAppearance = navBarAppereance
+//        UINavigationBar.appearance().compactAppearance = navBarAppereance
+//        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppereance
+//        UINavigationBar.appearance().tintColor = UIColor.systemBackground
+        
+        
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+                  UINavigationBar.appearance().shadowImage = UIImage()
+                  UINavigationBar.appearance().isTranslucent = true
+                  UINavigationBar.appearance().tintColor = .clear
+                  UINavigationBar.appearance().backgroundColor = .clear
+        
+    }
+    
     var body: some View {
         NavigationView {
-            List(datasetManager.equipmentList) {
-                equipment in
-                NavigationLink(destination: DetailsView(titleDate: equipment.date)) {
-                    Text(equipment.date)
-                    
+            List(datasetManager.personnelList.reversed()) {
+                losses in
+                VStack {
+                    Text(String(losses.day))
                 }
+                .listRowBackground(Color.clear)
+            }
+            .onAppear() {
+                self.datasetManager.fetchLossesData()
+                
             }
             .listStyle(.inset)
-            .navigationBarTitle("#шо_по_потерям")
-        }.onAppear {
-            self.datasetManager.fetchLossesData()
+            .background(Image("BackgroundImage").ignoresSafeArea(.all))
+            .navigationBarTitle("Ukraine Russia War", displayMode: .inline)
         }
+        
+        //            Image() {
+        //                NavigationView {
+        //                    List(datasetManager.personnelList.reversed()) {
+        //                        personnel in
+        //                        if personnel.day == 97 { // TODO: change condition
+        //
+        //                        }
+        //                        else {
+        //
+        //                            Text(String(personnel.day))
+        //                        }
+        //                    }
+        //                    .listStyle(.inset)
+        //                    .navigationBarTitle("#шо_по_потерям")
+        //                }.onAppear {
+        //                    self.datasetManager.fetchLossesData()
+        //                }
+        //            }
     }
 }
 
