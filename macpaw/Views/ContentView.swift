0201add63d5b5b2fb8cccd0444f01278e73bf544
AndrewShakula <attentionhub@gmail.com>
2022-07-22 14:02:44 +0300

    Changed colorScheme to get light content in status bar

--- macpaw/Views/ContentView.swift
+++ macpaw/Views/ContentView.swift
@@ -77,6 +77,7 @@ struct ContentView: View {
             .navigationBarTitle("Ukraine Russia War", displayMode: .inline)
             .searchable(text: $searchText)
             .foregroundColor(.gray)
+            .preferredColorScheme(.dark)
         }
         .onAppear() {
             self.datasetManager.fetchLossesData()
