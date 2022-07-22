c350c6f7b7932b3b8bc9684eba621de1f08d032f
AndrewShakula <attentionhub@gmail.com>
2022-07-22 16:04:28 +0300

    Refactor

--- macpaw/Controllers/DatasetManager.swift
+++ macpaw/Controllers/DatasetManager.swift
@@ -44,54 +44,29 @@ class DatasetManager: ObservableObject {
     func getLossesStats (personnel p: Personnel, equipment e: Equipment) -> [LossesStats] {
         var stats = [LossesStats]()
         
-        if let safePOW = p.prisonerOfWar, safePOW != 0 {
-            stats.append(LossesStats(title: "Військовополонені", value: String(safePOW)))
+        [
+            "Військовополонені": p.prisonerOfWar,
+            "Літаки": e.aircraft,
+            "Гелікоптери": e.helicopter,
+            "Танки": e.tank,
+            "ББМ": e.armoredPersonnelCarrier,
+            "Артсистеми": e.fieldArtillery,
+            "РСЗВ": e.multipleRocketLauncher,
+            "Автомобілі": e.militaryAuto,
+            "Цистерни з ПММ": e.fuelTank,
+            "БПЛА/Дрони": e.drone,
+            "Кораблі/Катери": e.navalShip,
+            "Засоби ППО": e.antiAircraftWarfare,
+            "Спецтехніка": e.specialEquipment,
+            "ПУ ОТРК/ТРК": e.mobileSRBMSystem,
+            "Автомобілі та автоцистерни": e.vehiclesAndFuelTanks,
+            "Крилаті ракети": e.cruiseMissiles
+        ].forEach { (title: String, value: Int?) in
+            if let safeValue = value, safeValue != 0 {
+                stats.append(LossesStats(title: title, value: String(safeValue)))
+            }
         }
-        if e.aircraft != 0 {
-            stats.append(LossesStats(title: "Літаки", value: String(e.aircraft)))
-        }
-        if e.helicopter != 0 {
-            stats.append(LossesStats(title: "Гелікоптери", value: String(e.helicopter)))
-        }
-        if e.tank != 0 {
-            stats.append(LossesStats(title: "Танки", value: String(e.tank)))
-        }
-        if e.armoredPersonnelCarrier != 0 {
-            stats.append(LossesStats(title: "ББМ", value: String(e.armoredPersonnelCarrier)))
-        }
-        if e.fieldArtillery != 0 {
-            stats.append(LossesStats(title: "Артсистеми", value: String(e.fieldArtillery)))
-        }
-        if e.multipleRocketLauncher != 0 {
-            stats.append(LossesStats(title: "РСЗВ", value: String(e.multipleRocketLauncher)))
-        }
-        if let safeMilAuto = e.militaryAuto, safeMilAuto != 0 {
-            stats.append(LossesStats(title: "Автомобілі", value: String(safeMilAuto)))
-        }
-        if let safeFuelTank = e.fuelTank, safeFuelTank != 0 {
-            stats.append(LossesStats(title: "Цистерни з ПММ", value: String(safeFuelTank)))
-        }
-        if e.drone != 0 {
-            stats.append(LossesStats(title: "БПЛА/Дрони", value: String(e.drone)))
-        }
-        if e.navalShip != 0 {
-            stats.append(LossesStats(title: "Кораблі/Катери", value: String(e.navalShip)))
-        }
-        if e.antiAircraftWarfare != 0 {
-            stats.append(LossesStats(title: "Засоби ППО", value: String(e.antiAircraftWarfare)))
-        }
-        if let safeSpecEquipment = e.specialEquipment, safeSpecEquipment != 0 {
-            stats.append(LossesStats(title: "Спецтехніка", value: String(safeSpecEquipment)))
-        }
-        if let safeSRBM = e.mobileSRBMSystem, safeSRBM != 0 {
-            stats.append(LossesStats(title: "ПУ ОТРК/ТРК", value: String(safeSRBM)))
-        }
-        if let safeVehicleFuel = e.vehiclesAndFuelTanks, safeVehicleFuel != 0 {
-            stats.append(LossesStats(title: "Автомобілі та автоцистерни", value: String(safeVehicleFuel)))
-        }
-        if let safeCruiseMissiles = e.cruiseMissiles, safeCruiseMissiles != 0 {
-            stats.append(LossesStats(title: "Крилаті ракети", value: String(safeCruiseMissiles)))
-        }
+        
         return stats
     }
     
